ff529ef1576a870f485064613284660cacbb1070
yuuchien.yang@gmail.com <yuuchien.yang@gmail.com>
2023-06-16 21:16:54 +0800

    import immer for deep clone state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import { Chart } from 'react-google-charts';
+import { useImmer } from 'use-immer';
 import data from './assets/data.json';
 
 function App() {
   const [swapLeft, setSwapLeft] = useState(true);
-  const [candidateLeft, setCandidateLeft] = useState('');
-  const [candidateRight, setCandidateRight] = useState('');
+  const [candidateLeft, setCandidateLeft] = useImmer({});
+  const [candidateRight, setCandidateRight] = useImmer({});
   const [candidates, setCandidates] = useState(data);
 
   const options = {
@@ -24,7 +25,9 @@ function App() {
           } flex flex-col items-center bg-blue-200 w-96 hover:bg-blue-100`}
           onClick={() => setSwapLeft(true)}
         >
-          {candidateLeft ? (
+          {Object.keys(candidateLeft).length === 0 ? (
+            '請選擇議員'
+          ) : (
             <>
               <p>{candidateLeft.姓名}</p>
               <p>{candidateLeft.總收入}</p>
@@ -33,12 +36,10 @@ function App() {
                 chartType="PieChart"
                 data={candidateLeft.pie}
                 options={options}
-                width={'80%'}
+                width={'100%'}
                 height={'250px'}
               />
             </>
-          ) : (
-            '請選擇議員'
           )}
         </div>
         <div
@@ -47,25 +48,25 @@ function App() {
           } flex flex-col items-center bg-red-200 w-96 hover:bg-red-100`}
           onClick={() => setSwapLeft(false)}
         >
-          {candidateRight ? (
+          {Object.keys(candidateRight).length === 0 ? (
+            '請選擇議員'
+          ) : (
             <>
               <p>{candidateRight.姓名}</p>
               <p>{candidateRight.總收入}</p>
               <p>{candidateRight.捐贈企業數}</p>
               <Chart
                 chartType="PieChart"
-                data={candidateLeft.pie}
+                data={candidateRight.pie}
                 options={options}
-                width={'80%'}
+                width={'100%'}
                 height={'250px'}
               />
             </>
-          ) : (
-            '請選擇議員'
           )}
         </div>
       </div>
-      <div className="flex flex-wrap gap-2 w-[1500px]">
+      <div className="flex flex-wrap gap-2 w-[1000px]">
         {candidates.map((candidate) => {
           return (
             <div
@@ -84,11 +85,11 @@ function App() {
                   return;
                 }
                 if (swapLeft) {
-                  setCandidateLeft({
-                    姓名: candidate.姓名,
-                    總收入: candidate.總收入,
-                    捐贈企業數: candidate.捐贈企業數,
-                    pie: [
+                  setCandidateLeft((draft) => {
+                    draft.姓名 = candidate.姓名;
+                    draft.總收入 = candidate.總收入;
+                    draft.捐贈企業數 = candidate.捐贈企業數;
+                    draft.pie = [
                       ['來源', '金額'],
                       [
                         '個人',
@@ -118,14 +119,14 @@ function App() {
                         '其他',
                         parseInt(candidate.其他收入.split(',').join('')),
                       ],
-                    ],
+                    ];
                   });
                 } else {
-                  setCandidateRight({
-                    姓名: candidate.姓名,
-                    總收入: candidate.總收入,
-                    捐贈企業數: candidate.捐贈企業數,
-                    pie: [
+                  setCandidateRight((draft) => {
+                    draft.姓名 = candidate.姓名;
+                    draft.總收入 = candidate.總收入;
+                    draft.捐贈企業數 = candidate.捐贈企業數;
+                    draft.pie = [
                       ['來源', '金額'],
                       [
                         '個人',
@@ -155,7 +156,7 @@ function App() {
                         '其他',
                         parseInt(candidate.其他收入.split(',').join('')),
                       ],
-                    ],
+                    ];
                   });
                 }
               }}
