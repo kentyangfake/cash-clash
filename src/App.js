fefeceaae045daec947cd873b83efdc3074700f3
yuuchien.yang@gmail.com <yuuchien.yang@gmail.com>
2023-06-16 15:35:36 +0800

    fixed ui display logic

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <div className="flex flex-col items-center">
-      <div className="flex m-10 gap-10 border border-green-300 h-96 justify-center">
+      <div className="flex m-10 gap-10 h-96 justify-center">
         <div
           className={`${
             swapLeft && 'border border-blue-500'
@@ -32,10 +32,11 @@ function App() {
           return (
             <div
               className={`${
-                (candidateLeft.姓名 === candidate.姓名 ||
-                  candidateRight.姓名 === candidate.姓名) &&
-                'bg-amber-300'
-              } flex hover:bg-amber-300 justify-center items-center bg-zinc-200 w-24 h-20 p-2`}
+                candidateLeft.姓名 === candidate.姓名 ||
+                candidateRight.姓名 === candidate.姓名
+                  ? 'bg-amber-300'
+                  : 'bg-zinc-200 hover:bg-zinc-100'
+              } flex justify-center items-center w-24 h-20 p-2`}
               key={candidate.姓名}
               onClick={() => {
                 if (
