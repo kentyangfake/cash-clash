f0ebbb429cfa0be99353e6f0efc5023763862f74
yuuchien.yang@gmail.com <yuuchien.yang@gmail.com>
2023-06-16 16:48:08 +0800

    featured pie chart, TODO: useImmer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Chart } from 'react-google-charts';
 import data from './assets/data.json';
 
 function App() {
@@ -6,6 +7,13 @@ function App() {
   const [candidateLeft, setCandidateLeft] = useState('');
   const [candidateRight, setCandidateRight] = useState('');
   const [candidates, setCandidates] = useState(data);
+
+  const options = {
+    legend: 'none',
+    pieSliceText: 'label',
+    title: '資金來源的相關性',
+    pieStartAngle: 100,
+  };
 
   return (
     <div className="flex flex-col items-center">
@@ -16,7 +24,22 @@ function App() {
           } flex flex-col items-center bg-blue-200 w-96 hover:bg-blue-100`}
           onClick={() => setSwapLeft(true)}
         >
-          {candidateLeft ? <p>{candidateLeft.姓名}</p> : '請選擇議員'}
+          {candidateLeft ? (
+            <>
+              <p>{candidateLeft.姓名}</p>
+              <p>{candidateLeft.總收入}</p>
+              <p>{candidateLeft.捐贈企業數}</p>
+              <Chart
+                chartType="PieChart"
+                data={candidateLeft.pie}
+                options={options}
+                width={'80%'}
+                height={'250px'}
+              />
+            </>
+          ) : (
+            '請選擇議員'
+          )}
         </div>
         <div
           className={`${
@@ -24,7 +47,22 @@ function App() {
           } flex flex-col items-center bg-red-200 w-96 hover:bg-red-100`}
           onClick={() => setSwapLeft(false)}
         >
-          {candidateRight ? <p>{candidateRight.姓名}</p> : '請選擇議員'}
+          {candidateRight ? (
+            <>
+              <p>{candidateRight.姓名}</p>
+              <p>{candidateRight.總收入}</p>
+              <p>{candidateRight.捐贈企業數}</p>
+              <Chart
+                chartType="PieChart"
+                data={candidateLeft.pie}
+                options={options}
+                width={'80%'}
+                height={'250px'}
+              />
+            </>
+          ) : (
+            '請選擇議員'
+          )}
         </div>
       </div>
       <div className="flex flex-wrap gap-2 w-[1500px]">
@@ -46,9 +84,79 @@ function App() {
                   return;
                 }
                 if (swapLeft) {
-                  setCandidateLeft({ 姓名: candidate.姓名 });
+                  setCandidateLeft({
+                    姓名: candidate.姓名,
+                    總收入: candidate.總收入,
+                    捐贈企業數: candidate.捐贈企業數,
+                    pie: [
+                      ['來源', '金額'],
+                      [
+                        '個人',
+                        parseInt(candidate.個人捐贈收入.split(',').join('')),
+                      ],
+                      [
+                        '營利事業',
+                        parseInt(
+                          candidate.營利事業捐贈收入.split(',').join('')
+                        ),
+                      ],
+                      [
+                        '政黨',
+                        parseInt(candidate.政黨捐贈收入.split(',').join('')),
+                      ],
+                      [
+                        '人民團體',
+                        parseInt(
+                          candidate.人民團體捐贈收入.split(',').join('')
+                        ),
+                      ],
+                      [
+                        '匿名',
+                        parseInt(candidate.匿名捐贈收入.split(',').join('')),
+                      ],
+                      [
+                        '其他',
+                        parseInt(candidate.其他收入.split(',').join('')),
+                      ],
+                    ],
+                  });
                 } else {
-                  setCandidateRight({ 姓名: candidate.姓名 });
+                  setCandidateRight({
+                    姓名: candidate.姓名,
+                    總收入: candidate.總收入,
+                    捐贈企業數: candidate.捐贈企業數,
+                    pie: [
+                      ['來源', '金額'],
+                      [
+                        '個人',
+                        parseInt(candidate.個人捐贈收入.split(',').join('')),
+                      ],
+                      [
+                        '營利事業',
+                        parseInt(
+                          candidate.營利事業捐贈收入.split(',').join('')
+                        ),
+                      ],
+                      [
+                        '政黨',
+                        parseInt(candidate.政黨捐贈收入.split(',').join('')),
+                      ],
+                      [
+                        '人民團體',
+                        parseInt(
+                          candidate.人民團體捐贈收入.split(',').join('')
+                        ),
+                      ],
+                      [
+                        '匿名',
+                        parseInt(candidate.匿名捐贈收入.split(',').join('')),
+                      ],
+                      [
+                        '其他',
+                        parseInt(candidate.其他收入.split(',').join('')),
+                      ],
+                    ],
+                  });
                 }
               }}
             >
